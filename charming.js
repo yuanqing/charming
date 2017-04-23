a7ed903ac6bb13ed46d4255e8dc5ab7b499b663e
Yuan Qing <yuanqing.lim@honestbee.com>
2017-04-23 17:14:22 +0800

    Rewrite

--- charming.js
+++ charming.js
@@ -1,49 +1,61 @@
-(function(fn) {
-  /* istanbul ignore if  */
-  if (typeof module === 'undefined') {
-    this.charming = fn;
-  } else {
-    module.exports = fn;
+;(function() {
+  function Charming(element, options) {
+    if (!(this instanceof Charming)) {
+      return new Charming(element, options)
+    }
+    options = options || {}
+    this.tagName = options.tagName || 'span'
+    this.classPrefix = options.classPrefix != null
+      ? options.classPrefix
+      : 'char'
+    this.count = 1
+    this.traverse(element)
+    return element
   }
-})(function(elem, opts) {
 
-  'use strict';
-
-  opts = opts || {};
-  var tagName = opts.tagName || 'span';
-  var classPrefix = opts.classPrefix != null ? opts.classPrefix : 'char';
-
-  var count = 1;
-
-  var inject = function(elem) {
-    var parentNode = elem.parentNode;
-    var str = elem.nodeValue;
-    var len = str.length;
-    var i = -1;
-    while (++i < len) {
-      var node = document.createElement(tagName);
-      if (classPrefix) {
-        node.className = classPrefix + count;
-        count++;
+  Charming.prototype.inject = function(element) {
+    var parentNode = element.parentNode
+    var string = element.nodeValue
+    var length = string.length
+    var i = -1
+    while (++i < length) {
+      var node = document.createElement(this.tagName)
+      if (this.classPrefix) {
+        node.className = this.classPrefix + this.count
+        this.count++
       }
-      node.appendChild(document.createTextNode(str[i]));
-      parentNode.insertBefore(node, elem);
+      node.appendChild(document.createTextNode(string[i]))
+      parentNode.insertBefore(node, element)
     }
-    parentNode.removeChild(elem);
-  };
+    parentNode.removeChild(element)
+    return element
+  }
 
-  (function traverse(elem) {
-    var childNodes = [].slice.call(elem.childNodes); // static array of nodes
-    var len = childNodes.length;
-    var i = -1;
-    while (++i < len) {
-      traverse(childNodes[i]);
+  Charming.prototype.traverse = function(element) {
+    // `element` is itself a text node.
+    if (element.nodeType === Node.TEXT_NODE) {
+      return this.inject(element)
     }
-    if (elem.nodeType === Node.TEXT_NODE) {
-      inject(elem);
+
+    // `element` has a single child text node.
+    var childNodes = Array.prototype.slice.call(element.childNodes) // static array of nodes
+    var length = childNodes.length
+    if (length === 1 && childNodes[0].nodeType === Node.TEXT_NODE) {
+      this.inject(childNodes[0])
+      return element
     }
-  })(elem);
 
-  return elem;
+    // `element` has more than one child node.
+    var i = -1
+    while (++i < length) {
+      this.traverse(childNodes[i])
+    }
+    return element
+  }
 
-});
+  if (typeof module === 'object') {
+    module.exports = Charming
+  } else {
+    this.charming = Charming
+  }
+})()
--- test/index.js
+++ test/index.js
@@ -1,64 +1,85 @@
-/* globals describe, it, expect */
-'use strict';
+const test = require('tape')
+const charming = require('../')
 
-var charming = require('../');
+function createElement(innerHTML) {
+  const elem = document.createElement('div')
+  elem.innerHTML = innerHTML || ''
+  document.body.appendChild(elem)
+  return elem
+}
 
-describe('charming(elem, opts)', function() {
+test('should not inject spans when `elem` has no child nodes', function(t) {
+  t.plan(1)
+  const elem = createElement()
+  charming(elem)
+  t.equal(elem.innerHTML, '')
+})
 
-  var createElement = function (innerHTML) {
-    var elem = document.createElement('div');
-    elem.innerHTML = innerHTML || '';
-    document.body.appendChild(elem);
-    return elem;
-  };
+test('should not inject spans when `elem` has no child text nodes', function(
+  t
+) {
+  t.plan(1)
+  const innerHTML = '<span></span>'
+  const elem = createElement(innerHTML)
+  charming(elem)
+  t.equal(elem.innerHTML, innerHTML)
+})
 
-  it('should not inject spans when `elem` has no child nodes', function() {
-    var elem = createElement();
-    charming(elem);
-    expect(elem.innerHTML).toMatch('');
-  });
+test('should inject spans when `elem` has a single child text node', function(
+  t
+) {
+  t.plan(1)
+  const elem = createElement('foo')
+  charming(elem)
+  t.equal(
+    elem.innerHTML,
+    '<span class="char1">f</span><span class="char2">o</span><span class="char3">o</span>'
+  )
+})
 
-  it('should not inject spans when `elem` has no child text nodes', function() {
-    var innerHTML = '<span></span>',
-      elem = createElement(innerHTML);
-    charming(elem);
-    expect(elem.innerHTML).toMatch(innerHTML);
-  });
+test('should inject spans when `elem` has multiple child text nodes', function(
+  t
+) {
+  t.plan(1)
+  const elem = createElement(
+    '<span>foo</span> <span>bar <span>baz</span></span>'
+  )
+  charming(elem)
+  t.equal(
+    elem.innerHTML,
+    '<span><span class="char1">f</span><span class="char2">o</span><span class="char3">o</span></span><span class="char4"> </span><span><span class="char5">b</span><span class="char6">a</span><span class="char7">r</span><span class="char8"> </span><span><span class="char9">b</span><span class="char10">a</span><span class="char11">z</span></span></span>'
+  )
+})
 
-  it('should inject spans when `elem` has a single child text node', function() {
-    var elem = createElement('foo');
-    charming(elem);
-    expect(elem.innerHTML).toMatch('<span class="char1">f</span><span class="char2">o</span><span class="char3">o</span>');
-  });
+test('can inject custom tags', function(t) {
+  t.plan(1)
+  const elem = createElement('foo')
+  charming(elem, {
+    tagName: 'b'
+  })
+  t.equal(
+    elem.innerHTML,
+    '<b class="char1">f</b><b class="char2">o</b><b class="char3">o</b>'
+  )
+})
 
-  it('should inject spans when `elem` has multiple child text nodes', function() {
-    var elem = createElement('<span>foo</span> <span>bar <span>baz</span></span>');
-    charming(elem);
-    expect(elem.innerHTML).toMatch('<span><span class="char1">f</span><span class="char2">o</span><span class="char3">o</span></span><span class="char4"> </span><span><span class="char5">b</span><span class="char6">a</span><span class="char7">r</span><span class="char8"> </span><span><span class="char9">b</span><span class="char10">a</span><span class="char11">z</span></span></span>');
-  });
+test('can inject spans without classes', function(t) {
+  t.plan(1)
+  const elem = createElement('foo')
+  charming(elem, {
+    classPrefix: false
+  })
+  t.equal(elem.innerHTML, '<span>f</span><span>o</span><span>o</span>')
+})
 
-  it('can inject custom tags', function() {
-    var elem = createElement('foo');
-    charming(elem, {
-      tagName: 'b'
-    });
-    expect(elem.innerHTML).toMatch('<b class="char1">f</b><b class="char2">o</b><b class="char3">o</b>');
-  });
-
-  it('can inject spans without classes', function() {
-    var elem = createElement('foo');
-    charming(elem, {
-      classPrefix: false
-    });
-    expect(elem.innerHTML).toMatch('<span>f</span><span>o</span><span>o</span>');
-  });
-
-  it('can inject spans with a custom class prefix', function() {
-    var elem = createElement('foo');
-    charming(elem, {
-      classPrefix: 'c'
-    });
-    expect(elem.innerHTML).toMatch('<span class="c1">f</span><span class="c2">o</span><span class="c3">o</span>');
-  });
-
-});
+test('can inject spans with a custom class prefix', function(t) {
+  t.plan(1)
+  const elem = createElement('foo')
+  charming(elem, {
+    classPrefix: 'c'
+  })
+  t.equal(
+    elem.innerHTML,
+    '<span class="c1">f</span><span class="c2">o</span><span class="c3">o</span>'
+  )
+})
