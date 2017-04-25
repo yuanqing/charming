3c201019193982704921f50fd7fc8ed01b9929ea
Yuan Qing <yuanqing.lim@honestbee.com>
2017-04-26 03:59:34 +0800

    Use plain functions

--- index.js
+++ index.js
@@ -1,55 +1,47 @@
-function Charming(element, options) {
-  if (!(this instanceof Charming)) {
-    return new Charming(element, options)
-  }
+module.exports = function(element, options) {
   options = options || {}
-  this.tagName = options.tagName || 'span'
-  this.classPrefix = options.classPrefix != null
-    ? options.classPrefix
-    : 'char'
-  this.count = 1
-  this.traverse(element)
-  return element
-}
 
-Charming.prototype.inject = function(element) {
-  var parentNode = element.parentNode
-  var string = element.nodeValue
-  var length = string.length
-  var i = -1
-  while (++i < length) {
-    var node = document.createElement(this.tagName)
-    if (this.classPrefix) {
-      node.className = this.classPrefix + this.count
-      this.count++
+  var tagName = options.tagName || 'span'
+  var classPrefix = options.classPrefix != null ? options.classPrefix : 'char'
+  var count = 1
+
+  function inject(element) {
+    var parentNode = element.parentNode
+    var string = element.nodeValue
+    var length = string.length
+    var i = -1
+    while (++i < length) {
+      var node = document.createElement(tagName)
+      if (classPrefix) {
+        node.className = classPrefix + count
+        count++
+      }
+      node.appendChild(document.createTextNode(string[i]))
+      parentNode.insertBefore(node, element)
     }
-    node.appendChild(document.createTextNode(string[i]))
-    parentNode.insertBefore(node, element)
-  }
-  parentNode.removeChild(element)
-  return element
-}
-
-Charming.prototype.traverse = function(element) {
-  // `element` is itself a text node.
-  if (element.nodeType === Node.TEXT_NODE) {
-    return this.inject(element)
-  }
-
-  // `element` has a single child text node.
-  var childNodes = Array.prototype.slice.call(element.childNodes) // static array of nodes
-  var length = childNodes.length
-  if (length === 1 && childNodes[0].nodeType === Node.TEXT_NODE) {
-    this.inject(childNodes[0])
+    parentNode.removeChild(element)
     return element
   }
 
-  // `element` has more than one child node.
-  var i = -1
-  while (++i < length) {
-    this.traverse(childNodes[i])
-  }
-  return element
-}
+  ;(function traverse(element) {
+    // `element` is itself a text node.
+    if (element.nodeType === Node.TEXT_NODE) {
+      return inject(element)
+    }
 
-module.exports = Charming
+    // `element` has a single child text node.
+    var childNodes = Array.prototype.slice.call(element.childNodes) // static array of nodes
+    var length = childNodes.length
+    if (length === 1 && childNodes[0].nodeType === Node.TEXT_NODE) {
+      inject(childNodes[0])
+      return element
+    }
+
+    // `element` has more than one child node.
+    var i = -1
+    while (++i < length) {
+      traverse(childNodes[i])
+    }
+    return element
+  })(element)
+}
